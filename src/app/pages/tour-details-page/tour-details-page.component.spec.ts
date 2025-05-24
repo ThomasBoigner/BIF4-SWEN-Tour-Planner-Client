1306d7196c52c1227aefdc9feb6392252cf74786
Thomas Boigner <thomasboigner@outlook.com>
2025-05-24 15:19:36 +0200

    Add tour-details-page.component.spec.ts

---
+++ src/app/pages/tour-details-page/tour-details-page.component.spec.ts
@@ -0,0 +1,75 @@
+import { TourService } from '../../service/tour.service';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { routes } from '../../app.routes';
+import { Tour } from '../../model/tour';
+import { of } from 'rxjs';
+import { TourDetailsPageComponent } from './tour-details-page.component';
+
+describe('TourDetailsPageComponent', () => {
+    let tourService: jasmine.SpyObj<TourService>;
+
+    beforeEach(() => {
+        const spy = jasmine.createSpyObj<TourService>('TourService', ['getTour']);
+        TestBed.configureTestingModule({
+            imports: [TourDetailsPageComponent],
+            providers: [{ provide: TourService, useValue: spy }, provideRouter(routes)],
+        });
+        tourService = TestBed.inject(TourService) as jasmine.SpyObj<TourService>;
+    });
+
+    it('Tour details should be displayed', () => {
+        // Given
+        const tour: Tour = {
+            id: 'e4f61472-1ead-4b0a-a895-c7ae75139fc2',
+            name: 'Tour 1',
+            description: 'This tour is awesome',
+            from: {
+                country: 'Austria',
+                city: 'Deutsch Wagram',
+                zipCode: 2232,
+                streetName: 'Radetzkystraße',
+                streetNumber: '2-6',
+            },
+            to: {
+                country: 'Austria',
+                city: 'Strasshof an der Nordbahn',
+                zipCode: 2231,
+                streetName: 'Billroth-Gasse',
+                streetNumber: '5',
+            },
+            transportType: 'BIKE',
+            distance: 20.0,
+            estimatedTime: 120.0,
+            imageUrl: 'img',
+        };
+
+        tourService.getTour.and.returnValue(of(tour));
+
+        // When
+
+        const fixture: ComponentFixture<TourDetailsPageComponent> =
+            TestBed.createComponent(TourDetailsPageComponent);
+
+        fixture.detectChanges();
+
+        // Then
+        const nativeElement = fixture.nativeElement as HTMLElement;
+        expect(fixture.componentInstance).toBeDefined();
+        expect(nativeElement.textContent).toContain(tour.name);
+        expect(nativeElement.textContent).toContain(tour.description);
+        expect(nativeElement.textContent).toContain(tour.from.streetName);
+        expect(nativeElement.textContent).toContain(tour.from.streetNumber);
+        expect(nativeElement.textContent).toContain(tour.from.zipCode);
+        expect(nativeElement.textContent).toContain(tour.from.city);
+        expect(nativeElement.textContent).toContain(tour.from.country);
+        expect(nativeElement.textContent).toContain(tour.to.streetName);
+        expect(nativeElement.textContent).toContain(tour.to.streetNumber);
+        expect(nativeElement.textContent).toContain(tour.to.zipCode);
+        expect(nativeElement.textContent).toContain(tour.to.city);
+        expect(nativeElement.textContent).toContain(tour.to.country);
+        expect(nativeElement.textContent).toContain(tour.distance);
+        expect(nativeElement.textContent).toContain(tour.estimatedTime / 60);
+        expect(nativeElement.textContent).toContain(tour.transportType);
+    });
+});
