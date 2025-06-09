ce3d50f576a320cb5b4fdaf497b515e4bf5c71a7
Thomas Boigner <thomasboigner@outlook.com>
2025-06-09 23:10:36 +0200

    Refactor tour list item to its own component

--- src/app/pages/tours-list-page/tours-list-page.component.ts
+++ src/app/pages/tours-list-page/tours-list-page.component.ts
@@ -4,12 +4,13 @@ import { TourService } from '../../service/tour.service';
 import { AsyncPipe, NgOptimizedImage } from '@angular/common';
 import { Observable } from 'rxjs';
 import { RouterLink } from '@angular/router';
+import { TourListItemComponent } from '../../components/tour-list-item/tour-list-item.component';
 
 @Component({
     selector: 'tours-list-page',
     templateUrl: './tours-list-page.component.html',
     styleUrls: ['./tours-list-page.component.css'],
-    imports: [NgOptimizedImage, AsyncPipe, RouterLink],
+    imports: [NgOptimizedImage, AsyncPipe, RouterLink, TourListItemComponent],
 })
 export class ToursListPageComponent {
     tours$: Observable<Tour[]>;
