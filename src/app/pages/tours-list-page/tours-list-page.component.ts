4b812274e3a317ce0acf6a9d54616ab72f4f74a1
Thomas Boigner <59614919+ThomasBoigner@users.noreply.github.com>
2025-06-16 10:49:36 +0200

    Add map
    Setup map using Leaflet with leaflet routing machine for routing

--- src/app/pages/tours-list-page/tours-list-page.component.ts
+++ src/app/pages/tours-list-page/tours-list-page.component.ts
@@ -7,6 +7,9 @@ import { RouterLink } from '@angular/router';
 import { TourListItemComponent } from '../../components/tour-list-item/tour-list-item.component';
 import { TourButtonComponent } from '../../components/tour-button/tour-button.component';
 import { SearchBarComponent } from '../../components/search-bar/search-bar.component';
+import { LeafletDirective } from '@bluehalo/ngx-leaflet';
+import L, { latLng, tileLayer, Map, Control } from 'leaflet';
+import 'leaflet-routing-machine';
 
 @Component({
     selector: 'tours-list-page',
@@ -19,12 +22,67 @@ import { SearchBarComponent } from '../../components/search-bar/search-bar.compo
         TourListItemComponent,
         TourButtonComponent,
         SearchBarComponent,
+        LeafletDirective,
     ],
 })
 export class ToursListPageComponent {
     tours$: Observable<Tour[]>;
+    map: Map | undefined;
+    control: Control | undefined;
+
+    leafletOptions = {
+        layers: [
+            tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 18,
+                attribution: '...',
+            }),
+        ],
+        zoom: 3,
+        center: latLng(0, 0),
+    };
 
     constructor(private tourService: TourService) {
         this.tours$ = this.tourService.getTours();
+    }
+
+    onMapReady(map: Map) {
+        this.map = map;
+    }
+
+    selectTour(tour: Tour) {
+        if (!this.map) {
+            return;
+        }
+
+        if (this.control) {
+            this.control.remove();
+        }
+
+        const plan = L.routing.plan(
+            [
+                latLng([tour.from.latitude, tour.from.longitude]),
+                latLng([tour.to.latitude, tour.to.longitude]),
+            ],
+            {
+                draggableWaypoints: false,
+                addWaypoints: false,
+            },
+        );
+
+        this.control = L.routing
+            .control({
+                plan: plan,
+                lineOptions: {
+                    styles: [
+                        { color: 'black', opacity: 0.15, weight: 9 },
+                        { color: 'white', opacity: 0.8, weight: 6 },
+                        { color: 'blue', opacity: 1, weight: 2 },
+                    ],
+                    addWaypoints: false,
+                    missingRouteTolerance: 1000,
+                    extendToWaypoints: true,
+                },
+            })
+            .addTo(this.map);
     }
 }
