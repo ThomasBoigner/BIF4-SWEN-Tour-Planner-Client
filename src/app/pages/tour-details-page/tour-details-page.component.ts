d75feff4232ef24344681afd15157307988ea68a
Thomas Boigner <thomasboigner@outlook.com>
2025-06-24 16:12:06 +0200

    Add support for paging get tour logs endpoint

--- src/app/pages/tour-details-page/tour-details-page.component.ts
+++ src/app/pages/tour-details-page/tour-details-page.component.ts
@@ -9,6 +9,7 @@ import { TourLog } from '../../model/tour-log';
 import { TourLogListItemComponent } from '../../components/tour-log-list-item/tour-log-list-item.component';
 import { TourButtonComponent } from '../../components/tour-button/tour-button.component';
 import { SearchBarComponent } from '../../components/search-bar/search-bar.component';
+import { Page } from '../../model/page';
 
 @Component({
     selector: 'tour-details-page',
@@ -26,8 +27,10 @@ import { SearchBarComponent } from '../../components/search-bar/search-bar.compo
 })
 export class TourDetailsPageComponent {
     tour$: Observable<Tour>;
-    tourLog$: Observable<TourLog[]>;
     tourId: string;
+
+    tourLogPages: Page<TourLog>[];
+    searchInput = '';
 
     constructor(
         private tourService: TourService,
@@ -37,12 +40,26 @@ export class TourDetailsPageComponent {
     ) {
         this.tourId = this.route.snapshot.paramMap.get('id') ?? '';
         this.tour$ = this.tourService.getTour(this.tourId);
-        this.tourLog$ = this.tourLogService.getTourLogsForTour(this.tourId);
+        this.tourLogPages = [];
+        this.tourLogService
+            .getTourLogsForTour(this.tourId, this.searchInput, 0, 5)
+            .subscribe((tourLog) => {
+                if (tourLog) {
+                    this.tourLogPages.push(tourLog);
+                }
+            });
     }
 
     deleteTourLog(id: string) {
         this.tourLogService.deleteTourLog(id).subscribe(() => {
-            this.tourLog$ = this.tourLogService.getTourLogsForTour(this.tourId);
+            this.tourLogPages = [];
+            this.tourLogService
+                .getTourLogsForTour(this.tourId, this.searchInput, 0, 5)
+                .subscribe((tourLog) => {
+                    if (tourLog) {
+                        this.tourLogPages.push(tourLog);
+                    }
+                });
         });
     }
 
