e90fdd7b529c4a56d899cd2ba0ac5a13c295ff93
Thomas Boigner <thomasboigner@outlook.com>
2025-06-26 17:31:02 +0200

    Add import functionality

--- src/app/service/backup.service.ts
+++ src/app/service/backup.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { NGXLogger } from 'ngx-logger';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class BackupService {
@@ -13,26 +14,19 @@ export class BackupService {
         this.backupUrl = 'http://localhost:8080/api/backup';
     }
 
-    public backupTour(id: string) {
+    public exportTour(id: string) {
         this.logger.debug(`Trying to backup tour with id ${id}`);
-        this.http
-            .get(`${this.backupUrl}/export/${id}`, {
-                responseType: 'blob',
-                observe: 'response',
-            })
-            .subscribe((response) => {
-                if (!response.body) {
-                    return;
-                }
+        return this.http.get(`${this.backupUrl}/export/${id}`, {
+            responseType: 'blob',
+            observe: 'response',
+        });
+    }
 
-                const contentDisposition = response.headers.get('content-disposition');
-                const match = contentDisposition?.match(/filename="?(.+?)"?$/);
+    public importTour(file: File): Observable<object> {
+        this.logger.debug(`Trying to restore tour with file ${file.name}`);
 
-                const url = window.URL.createObjectURL(response.body);
-                const a = document.createElement('a');
-                a.href = url;
-                a.download = match ? match[1] : 'backup.json';
-                a.click();
-            });
+        const formData = new FormData();
+        formData.append('file', file);
+        return this.http.post(`${this.backupUrl}/import`, formData);
     }
 }
