ba3be8f824f6e8e4f46aeda2f0e871a2a7353457
Thomas Boigner <thomasboigner@outlook.com>
2025-06-26 23:04:51 +0200

    Add basic html and css of update tour log page

--- src/app/components/tour-log-list-item/tour-log-list-item.component.ts
+++ src/app/components/tour-log-list-item/tour-log-list-item.component.ts
@@ -1,12 +1,13 @@
 import { Component, input, output } from '@angular/core';
 import { TourLog } from '../../model/tour-log';
 import { DatePipe, NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'tour-log-list-item',
     templateUrl: './tour-log-list-item.component.html',
     styleUrls: ['./tour-log-list-item.component.css'],
-    imports: [NgOptimizedImage, DatePipe],
+    imports: [NgOptimizedImage, DatePipe, RouterLink],
 })
 export class TourLogListItemComponent {
     tourLog = input.required<TourLog>();
---
+++ src/app/pages/update-tour-log-page/update-tour-log-page.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { ErrorMessageComponent } from '../../components/error-message/error-message.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { TourLogService } from '../../service/tour-log.service';
+import { DateInputComponent } from '../../components/date-input/date-input.component';
+import { TextInputComponent } from '../../components/text-input/text-input.component';
+import { TourButtonComponent } from '../../components/tour-button/tour-button.component';
+
+@Component({
+    selector: 'update-tour-log-page',
+    templateUrl: './update-tour-log-page.component.html',
+    styleUrls: ['./update-tour-log-page.component.css'],
+    imports: [
+        NgOptimizedImage,
+        RouterLink,
+        ErrorMessageComponent,
+        ReactiveFormsModule,
+        DateInputComponent,
+        TextInputComponent,
+        TourButtonComponent,
+    ],
+})
+export class UpdateTourLogPageComponent {
+    errorMessage = '';
+
+    tourLogForm = new FormGroup({
+        startTime: new FormControl<string>('', { nonNullable: true }), // ISO string or datetime-local input
+        endTime: new FormControl<string>('', { nonNullable: true }),
+        comment: new FormControl<string>('', { nonNullable: true }),
+        difficulty: new FormControl<number>(1, { nonNullable: true }),
+        distance: new FormControl<number>(0, { nonNullable: true }),
+        rating: new FormControl<number>(1, { nonNullable: true }),
+    });
+
+    constructor(
+        private tourLogService: TourLogService,
+        private route: ActivatedRoute,
+        private router: Router,
+    ) {}
+}
